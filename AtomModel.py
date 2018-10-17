0ab3f5c510393c2482a499c95dbab0709651cd43
bcwang <m18380581296@163.com>
2018-10-17 16:39:47 +0800

    0

--- AtomModel.py
+++ AtomModel.py
@@ -159,6 +159,7 @@ class FullAtomModel(object):
 
 
     def check_atom_feature_dict(self,atom_feature_dict):
+        return # 没有必要在非开发阶段进行检查
         if not isinstance(atom_feature_dict,dict):
             raise ValueError("Input must be a dict like 'H':array,'O':array")
         keys = atom_feature_dict.keys()
@@ -183,14 +184,15 @@ class FullAtomModel(object):
     def fit(self,x,y,epoch,batch_size=None,lr=0.0001,save_weights=True,load_weights=True):
 
         # 在这里判断是否所有的原子都包括在内
-        string = ""
-        for atom_type in x:
-            string += atom_type
-            string += str(x[atom_type].shape[1])
-        print_file(">>Train for %s" % string)
+        #string = ""
+        #for atom_type in x:
+        #    string += atom_type
+        #    string += str(x[atom_type].shape[1])
+        #print_file(">>Train for %s" % string)
 
 
-
+        if len(y) == 0:# 可能因为batch没有分好而出现空的情况
+            return
 
         y = np.array(y).reshape(-1,1)
         # batch_size 还没有实现
@@ -280,190 +282,192 @@ class FullAtomModel(object):
         print_file(str(self.sess.run(tf.trainable_variables()[0]).flatten()[:10]))
 
 
-def t_pred_save_load_weights():
-    a = FullAtomModel(["C", "H", "O", "Ni"], os.getcwd() + "/model", 4)
+def old_test():
+
+    def t_pred_save_load_weights():
+        a = FullAtomModel(["C", "H", "O", "Ni"], os.getcwd() + "/model", 4)
 
 
-    C = H = O = Ni = np.array([1,1,1,1]).reshape(1,1,4)
-    dict_ = {
-        "C":C,
-        "H":H,
-        "O":O,
-        "Ni":Ni
-    }
+        C = H = O = Ni = np.array([1,1,1,1]).reshape(1,1,4)
+        dict_ = {
+            "C":C,
+            "H":H,
+            "O":O,
+            "Ni":Ni
+        }
 
-    print(a.predict(dict_))
+        print(a.predict(dict_))
 
-    C = H = O  = np.array([1, 1, 1, 1,2,2,2,2,1,1,1,1,2,2,2,2]).reshape(2, 2, 4)
-    Ni = np.array([1, 1, 1, 1, 2, 2, 2, 2, ]).reshape(2, 1, 4)
-    dict_ = {
-        "C": C,
-        "H": H,
-        "O": O,
-        "Ni": Ni
+        C = H = O  = np.array([1, 1, 1, 1,2,2,2,2,1,1,1,1,2,2,2,2]).reshape(2, 2, 4)
+        Ni = np.array([1, 1, 1, 1, 2, 2, 2, 2, ]).reshape(2, 1, 4)
+        dict_ = {
+            "C": C,
+            "H": H,
+            "O": O,
+            "Ni": Ni
 
-    }
+        }
 
-    print(a.predict(dict_))
+        print(a.predict(dict_))
 
-    #tf.set_random_seed(123)
+        #tf.set_random_seed(123)
 
-    try:
+        try:
+            a.debug_print_weights()
+            #a.load_atom_weights()
+            a.debug_print_weights()
+            print("These two weighs should be different!")
+        except:
+            pass
+        y = [0]
+        a.fit(dict_,y,epoch=10)
         a.debug_print_weights()
-        #a.load_atom_weights()
-        a.debug_print_weights()
-        print("These two weighs should be different!")
-    except:
-        pass
-    y = [0]
-    a.fit(dict_,y,epoch=10)
-    a.debug_print_weights()
-    a.save_atom_weights()
+        a.save_atom_weights()
 
-def t_train():
-    # 这里atom cases也可以为原子序数
-    a = FullAtomModel(["C", "H", "O", "Ni"], os.getcwd() + "/model", 4)
+    def t_train():
+        # 这里atom cases也可以为原子序数
+        a = FullAtomModel(["C", "H", "O", "Ni"], os.getcwd() + "/model", 4)
 
-    C = H = O = Ni = np.array([1, 2, 3, 4]).reshape(1, 1, 4)
-    dict_ = {
-        "C": C,
-        "H": H,
-        "O": O,
-        "Ni": Ni
-    }
+        C = H = O = Ni = np.array([1, 2, 3, 4]).reshape(1, 1, 4)
+        dict_ = {
+            "C": C,
+            "H": H,
+            "O": O,
+            "Ni": Ni
+        }
 
-    print(a.predict(dict_))
+        print(a.predict(dict_))
 
-    y = [0.1]
+        y = [0.1]
 
-    print(a.fit(dict_,y,epoch=1000,load_weights=False))
+        print(a.fit(dict_,y,epoch=1000,load_weights=False))
 
-    print(a.predict(dict_))
+        print(a.predict(dict_))
 
-def train_from_pkl():
-    with open("C:\\Users\Administrator\Desktop\\feature.pkl", "rb") as f:
-        total_train_feed_x, total_test_feed_x, total_train_feed_y, total_test_feed_y, atom_cases, n_feat = pickle.load(f)
+    def train_from_pkl():
+        with open("C:\\Users\Administrator\Desktop\\feature.pkl", "rb") as f:
+            total_train_feed_x, total_test_feed_x, total_train_feed_y, total_test_feed_y, atom_cases, n_feat = pickle.load(f)
 
-    nn = FullAtomModel(atom_cases, os.getcwd() + "/model",n_feat)
+        nn = FullAtomModel(atom_cases, os.getcwd() + "/model",n_feat)
 
 
-    # 两种数据集交替训练
-    for _ in range(30):
-        nn.fit(total_train_feed_x[0],total_train_feed_y[0],epoch=100,load_weights=False)
-        nn.fit(total_train_feed_x[1], total_train_feed_y[1], epoch=100, load_weights=False)
+        # 两种数据集交替训练
+        for _ in range(30):
+            nn.fit(total_train_feed_x[0],total_train_feed_y[0],epoch=100,load_weights=False)
+            nn.fit(total_train_feed_x[1], total_train_feed_y[1], epoch=100, load_weights=False)
 
-    pred = nn.predict(total_test_feed_x[0])
+        pred = nn.predict(total_test_feed_x[0])
 
-    plt.plot(pred,total_test_feed_y[0],'ro')
-    plt.show()
+        plt.plot(pred,total_test_feed_y[0],'ro')
+        plt.show()
 
-    # 预测另一个完全不同的吸附质分子
-    pred = nn.predict(total_test_feed_x[1])
+        # 预测另一个完全不同的吸附质分子
+        pred = nn.predict(total_test_feed_x[1])
 
-    plt.plot(pred, total_test_feed_y[1], 'ro')
-    plt.show()
+        plt.plot(pred, total_test_feed_y[1], 'ro')
+        plt.show()
 
 
 
 
-def one_line_train():
+    def one_line_train():
 
-    '''
-    TODO: 数据集随机打乱训练
-    TODO：增加类存储，增加数据集，所涉及原子种类信息保存等，对NN结构进行调参
-    
-    '''
+        '''
+        TODO: 数据集随机打乱训练
+        TODO：增加类存储，增加数据集，所涉及原子种类信息保存等，对NN结构进行调参
 
-    print_file(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>New Game Begin!<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
-    print_file("Start Data Collecting")
-    # 准备数据
+        '''
 
-    aim_vasp_path = "S:\数据集\碳纳米管掺杂\\5-5\\b\oh"
+        print_file(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>New Game Begin!<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+        print_file("Start Data Collecting")
+        # 准备数据
 
-    dataset_maker = DatasetMaker(aim_vasp_path)
-    dataset_maker.make_dataset()
-    total_info = dataset_maker.give_out_dataset()
-    print_file("Finished Data Collecting, Start Feature Transform")
+        aim_vasp_path = "S:\数据集\碳纳米管掺杂\\5-5\\b\oh"
 
-    # 坐标编码
-    dataset_offer = DatasetOffer(total_data_info=total_info)
-    total_train_feed_x, \
-    total_test_feed_x, \
-    total_train_feed_y, \
-    total_test_feed_y, \
-    atom_cases, \
-    n_feat = \
-        dataset_offer.ANI_transform(save_pkl_path="ANI_features.pkl")
-    # 利用DeepChem的ANI transform进行转化，存储到pkl文件中
-    nn = FullAtomModel(atom_cases, os.getcwd() + "/model",n_feat)
+        dataset_maker = DatasetMaker(aim_vasp_path)
+        dataset_maker.make_dataset()
+        total_info = dataset_maker.give_out_dataset()
+        print_file("Finished Data Collecting, Start Feature Transform")
+
+        # 坐标编码
+        dataset_offer = DatasetOffer(total_data_info=total_info)
+        total_train_feed_x, \
+        total_test_feed_x, \
+        total_train_feed_y, \
+        total_test_feed_y, \
+        atom_cases, \
+        n_feat = \
+            dataset_offer.ANI_transform(save_pkl_path="ANI_features.pkl")
+        # 利用DeepChem的ANI transform进行转化，存储到pkl文件中
+        nn = FullAtomModel(atom_cases, os.getcwd() + "/model",n_feat)
 
 
-    try:
+        try:
+            nn.load_atom_weights()
+        except:
+            print_file("Load Weights Failed")
+
+        string = 'Total feed X shape: \n'
+        string += "Train: "
+
+        for i in total_train_feed_x:
+            for j in i:
+                print(i[j])
+                string += j + ":" +str(i[j].shape)
+        string += "\n"
+
+        string += "Test: "
+        for i in total_test_feed_x:
+            for j in i:
+                string += j + ":" + str(i[j].shape)
+        string += "\n"
+        print_file(string)
+
+        repeat = 100
+        index = 0
+
+        for i in range(repeat):# 这里用while True也行，因为每次fit都会保存weights
+            #print_file(">>Loop %s/%s"%(i+1,repeat))
+            print_file(">>Loop %s"%(index))
+
+            for dataset_index in range(len(total_train_feed_x)):
+                print_file(">>>>Train for %s/%s" % (dataset_index+1, len(total_train_feed_x)))
+                nn.fit(total_train_feed_x[dataset_index],total_train_feed_y[dataset_index],epoch=1000,load_weights=True)
+            index += 1
+
+        nn.save_atom_weights()
+
+    def predict():
+
+        # 直接使用编码后的数据，也可以现进行编码
+        with open("ANI_features.pkl", "rb") as f:
+            # _代表不使用train的数据
+            _, total_test_feed_x, _, total_test_feed_y, atom_cases, n_feat\
+                = pickle.load(f)
+
+        # 读取存储的模型
+        nn = FullAtomModel(atom_cases, os.getcwd() + "/model/trained", n_feat)
+
         nn.load_atom_weights()
-    except:
-        print_file("Load Weights Failed")
 
-    string = 'Total feed X shape: \n'
-    string += "Train: "
-
-    for i in total_train_feed_x:
-        for j in i:
-            print(i[j])
-            string += j + ":" +str(i[j].shape)
-    string += "\n"
-
-    string += "Test: "
-    for i in total_test_feed_x:
-        for j in i:
-            string += j + ":" + str(i[j].shape)
-    string += "\n"
-    print_file(string)
-
-    repeat = 100
-    index = 0
-
-    for i in range(repeat):# 这里用while True也行，因为每次fit都会保存weights
-        #print_file(">>Loop %s/%s"%(i+1,repeat))
-        print_file(">>Loop %s"%(index))
-
-        for dataset_index in range(len(total_train_feed_x)):
-            print_file(">>>>Train for %s/%s" % (dataset_index+1, len(total_train_feed_x)))
-            nn.fit(total_train_feed_x[dataset_index],total_train_feed_y[dataset_index],epoch=1000,load_weights=True)
-        index += 1
-
-    nn.save_atom_weights()
-
-def predict():
-
-    # 直接使用编码后的数据，也可以现进行编码
-    with open("ANI_features.pkl", "rb") as f:
-        # _代表不使用train的数据
-        _, total_test_feed_x, _, total_test_feed_y, atom_cases, n_feat\
-            = pickle.load(f)
-
-    # 读取存储的模型
-    nn = FullAtomModel(atom_cases, os.getcwd() + "/model/trained", n_feat)
-
-    nn.load_atom_weights()
-
-    pred_result = []
-    true_result = []
-    for dataset_index in range(len(total_test_feed_x)):
-        pred_result.extend(nn.predict(total_test_feed_x[dataset_index]))
-        true_result.extend(total_test_feed_y[dataset_index])
+        pred_result = []
+        true_result = []
+        for dataset_index in range(len(total_test_feed_x)):
+            pred_result.extend(nn.predict(total_test_feed_x[dataset_index]))
+            true_result.extend(total_test_feed_y[dataset_index])
 
 
-    plt.plot(pred_result,true_result,'ro')
-    plt.savefig("test_result.png",dpi=300)
-    plt.show()
+        plt.plot(pred_result,true_result,'ro')
+        plt.savefig("test_result.png",dpi=300)
+        plt.show()
 
 
 
 
-def show_weights():
-    a = FullAtomModel(["C", "H", "O", "Pt"], os.getcwd() + "/model/trained", 769)
-    a.load_atom_weights()
-    a.debug_print_weights()
+    def show_weights():
+        a = FullAtomModel(["C", "H", "O", "Pt"], os.getcwd() + "/model/trained", 769)
+        a.load_atom_weights()
+        a.debug_print_weights()
 
 
 
@@ -472,9 +476,9 @@ if __name__ == '__main__':
     #t_init_pred_save_load_weights()
     #t_train()
     #train_from_pkl()
-    one_line_train()
-    predict()
-
+    #one_line_train()
+    #predict()
+    pass
     #t_pred_save_load_weights()
     #show_weights()
 
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from AtomModel import *
+import numpy as np
+import traceback
 
 
 # 从Vasp文件夹中准备数据集，并将其用ANItransform转化，得到编码后的向量存储到pkl文件中
 def prepare_data_set(vasp_dir_path):
-    aim_vasp_path = vasp_dir_path # "S:\数据集\碳纳米管掺杂\\5-5\\b\oh"
+    aim_vasp_path = vasp_dir_path  # "S:\数据集\碳纳米管掺杂\\5-5\\b\oh"
 
     print_file(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>New Game Begin!<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
     print_file("Start Data Collecting")
@@ -23,14 +25,14 @@ def prepare_data_set(vasp_dir_path):
     n_feat = \
         dataset_offer.ANI_transform(save_pkl_path="ANI_features.pkl")
 
-# 将ANI编码过后的feature用于训练，存储模型到/model中
-def load_pkl_file_to_train(ANI_pkl_file_path="ANI_features.pkl",epoch=100):
 
-    nn = FullAtomModel(atom_cases, os.getcwd() + "/model", n_feat)
+# 将ANI编码过后的feature用于训练，存储模型到/model中
+def load_pkl_file_to_train(ANI_pkl_file_path="ANI_features.pkl", epoch=100):
     with open(ANI_pkl_file_path, "rb") as f:
         # _代表不使用train的数据
-        total_train_feed_x, _, total_train_feed_y, _, atom_cases, n_feat\
+        total_train_feed_x, total_test_feed_x, total_train_feed_y, total_test_feed_y, atom_cases, n_feat \
             = pickle.load(f)
+    nn = FullAtomModel(atom_cases, os.getcwd() + "/model", n_feat)
     try:
         nn.load_atom_weights()
     except:
@@ -40,6 +42,7 @@ def load_pkl_file_to_train(ANI_pkl_file_path="ANI_features.pkl",epoch=100):
     string += "Train: "
 
     for i in total_train_feed_x:
+        string += "\n"
         for j in i:
             print(i[j])
             string += j + ":" + str(i[j].shape)
@@ -47,31 +50,118 @@ def load_pkl_file_to_train(ANI_pkl_file_path="ANI_features.pkl",epoch=100):
 
     string += "Test: "
     for i in total_test_feed_x:
+        string += "\n"
         for j in i:
             string += j + ":" + str(i[j].shape)
     string += "\n"
     print_file(string)
 
 
-    index = 0
-    for i in range(epoch):# 这里用while True也行，因为每次fit都会保存weights
-        #print_file(">>Loop %s/%s"%(i+1,repeat))
-        print_file(">>Loop %s"%(index))
 
+    def train_group_by_dataset_from():  # 根据数据来源（vasp文件夹）划分，训练时容易过拟合
+        index = 0
+        for i in range(epoch):  # 这里用while True也行，因为每次fit都会保存weights
+            # print_file(">>Loop %s/%s"%(i+1,repeat))
+            print_file(">>Loop %s" % (index))
+
+            for dataset_index in range(len(total_train_feed_x)):
+                print_file(">>>>Train for %s/%s" % (dataset_index + 1, len(total_train_feed_x)))
+                nn.fit(total_train_feed_x[dataset_index], total_train_feed_y[dataset_index], epoch=1000,
+                       load_weights=True)
+            index += 1
+
+    def train_shuffle_by_batch_size(batch_size=32):
+        X = {}
+        y = np.array([])
+        # key随便来源一个数据集
+        atom_cases = list(total_train_feed_x[0].keys())
+        print(atom_cases)
         for dataset_index in range(len(total_train_feed_x)):
-            print_file(">>>>Train for %s/%s" % (dataset_index+1, len(total_train_feed_x)))
-            nn.fit(total_train_feed_x[dataset_index],total_train_feed_y[dataset_index],epoch=1000,load_weights=True)
-        index += 1
+            print("Dataset have samples: %s" % len(total_train_feed_y[dataset_index]))
+            '''
+            total_train_feed_x是一个List
+            每个例如以O C H 等元素为Key，之后为样本数量，原子数目和Feature长度！所以应当分别截取各个元素的前batchsize个
+            
+            
+            
+            '''
+
+            # 进行解包数据聚合，需要内存较大，如果样本来源很多需要修改处理方法
+            for atom in atom_cases:
+                try:
+                    X[atom] = np.concatenate([total_train_feed_x[dataset_index][atom], X[atom]], axis=0)
+                except KeyError:
+                    X[atom] = total_train_feed_x[dataset_index][atom]
+
+            y = np.concatenate([y,total_train_feed_y[dataset_index].reshape(-1)])
+
+        print("Y: ",y.shape)
+        print("X: ")
+        for atom in atom_cases:
+            print(X[atom].shape)# 这里需要和前面输出的total data sample一起看是否有遗漏
+
+        # 进行shuffle
+        shuffle_index = np.array(list(range(y.shape[0])))
+        print(np.random.shuffle(shuffle_index))
+
+        for atom in atom_cases:
+            X[atom] = X[atom][shuffle_index]
+        print("Before Shuffle: ",y[:10])
+        y = y[shuffle_index]
+        print("After shuffle: ",y[:10])
+        sample_numbers = y.shape[0]
+
+
+        # 开始重新分批次，每次一个batchsize
+        batch_num = int(sample_numbers / batch_size)+1
+        newX = {}
+        newY = {}
+        '''
+        这里和之前的数据结构不同了，之前是list，现在是dict
+        
+        '''
+        for i in range(batch_num-1):
+
+            for atom in atom_cases:
+                try:
+                    newX[i][atom] = X[atom][i*batch_size:1*batch_size+batch_size]
+                except:
+                    newX[i] = {}
+                    newX[i][atom] = X[atom][i*batch_size:1*batch_size+batch_size]
+
+            newY[i] = y[i*batch_size:1*batch_size+batch_size]
+        # 还剩下一个残余的batch
+        newX[batch_num-1] = X[atom][(batch_num-1)*batch_size:]
+        newY[batch_num-1] = y[batch_num*batch_size:]
+
+        # 剩下的训练就和之前的一样了
+        X = newX
+        y = newY
+        # 这里再显示一下信息
+
+        index = 0
+        while 1:
+            index += 1
+            print_file(">>Loop %s" % (index))
+            for dataset_index in X:
+                print(X[dataset_index])
+                print(y[dataset_index])
+                nn.fit(X[dataset_index], y[dataset_index], epoch=1000,
+                       load_weights=True)
+
+
+
+
+    train_shuffle_by_batch_size()
 
     nn.save_atom_weights()
 
 
 def predict(ANI_pkl_file_path="ANI_features.pkl"):
-
     # 直接使用编码后的数据，也可以现进行编码
     with open(ANI_pkl_file_path, "rb") as f:
         # _代表不使用train的数据
-        _, total_test_feed_x, _, total_test_feed_y, atom_cases, n_feat\
+        _, total_test_feed_x, _, total_test_feed_y, atom_cases, n_feat \
             = pickle.load(f)
 
     # 读取存储的模型
@@ -85,11 +175,10 @@ def predict(ANI_pkl_file_path="ANI_features.pkl"):
         pred_result.extend(nn.predict(total_test_feed_x[dataset_index]))
         true_result.extend(total_test_feed_y[dataset_index])
 
-
-    plt.plot(pred_result,true_result,'ro')
-    plt.savefig("test_result.png",dpi=300)
+    plt.plot(pred_result, true_result, 'ro')
+    plt.savefig("test_result.png", dpi=300)
     plt.show()
 
 
-
-prepare_data_set("/public/home/yangbo1/wangbch/nanotube/9-9")
+# prepare_data_set("/public/home/yangbo1/wangbch/nanotube/9-9")
+load_pkl_file_to_train()
